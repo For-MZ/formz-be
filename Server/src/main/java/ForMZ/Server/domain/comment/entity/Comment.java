2726691de553be1ae48d5f355376cb74a1a8af30
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-02-13 14:28:09 +0900

    Feat : Comment 도메인 Entity 1차 작업

--- Server/src/main/java/ForMZ/Server/domain/comment/entity/Comment.java
+++ Server/src/main/java/ForMZ/Server/domain/comment/entity/Comment.java
@@ -1,4 +1,39 @@
 package ForMZ.Server.domain.comment.entity;
 
+import ForMZ.Server.domain.like.commentLike.entity.CommentLike;
+import ForMZ.Server.domain.post.entity.Post;
+import ForMZ.Server.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Integer id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
+    private Post post;
+
+    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    private List<Comment> commentChild = new ArrayList<>();
+
+    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    private List<CommentLike> commentLikes = new ArrayList<>();
 }
