f84a433e018dc6ff7b5f1cb479bd50048c0f9389
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-02-13 14:28:39 +0900

    Feat : PostLike 도메인 Entity 1차 작업

--- Server/src/main/java/ForMZ/Server/domain/like/postLike/entity/PostLike.java
+++ Server/src/main/java/ForMZ/Server/domain/like/postLike/entity/PostLike.java
@@ -1,4 +1,26 @@
 package ForMZ.Server.domain.like.postLike.entity;
 
+import ForMZ.Server.domain.post.entity.Post;
+import ForMZ.Server.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "postLike_id")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
+    private Post post;
 }
