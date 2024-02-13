ccd9dbe339e9b61628bfe64f174312c31caeed6d
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-02-13 14:28:18 +0900

    Feat : Category 도메인 Entity 1차 작업

--- Server/src/main/java/ForMZ/Server/domain/category/entity/Category.java
+++ Server/src/main/java/ForMZ/Server/domain/category/entity/Category.java
@@ -1,4 +1,27 @@
 package ForMZ.Server.domain.category.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer id;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryCode categoryCode;
+
+    public enum CategoryCode{
+        YOUTH_POLICY,
+        YOUTH_SPACE,
+        HAPPY_HOUSE,
+        EMPLOYMENT,
+        FOUNDED
+    }
 }
