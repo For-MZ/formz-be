61d6cb2ab66b7b7a7070b4e2725b90db1103ef88
dev_ins <pis03133@naver.com>
2024-02-26 15:31:13 +0900

    Fix: comment Entity 임시 수정

--- Server/src/main/java/ForMZ/Server/domain/comment/entity/Comment.java
+++ Server/src/main/java/ForMZ/Server/domain/comment/entity/Comment.java
@@ -1,14 +1,10 @@
 package ForMZ.Server.domain.comment.entity;
 
-import ForMZ.Server.domain.like.commentLike.entity.CommentLike;
-import ForMZ.Server.domain.post.entity.Post;
+import ForMZ.Server.domain.post.Entity.Post;
 import ForMZ.Server.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -31,9 +27,9 @@ public class Comment {
     @JoinColumn(name = "POST_ID")
     private Post post;
 
-    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
-    private List<Comment> commentChild = new ArrayList<>();
+    //@OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    //private List<Comment> commentChild = new ArrayList<>();
 
-    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
-    private List<CommentLike> commentLikes = new ArrayList<>();
+    //@OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    //private List<CommentLike> commentLikes = new ArrayList<>();
 }
