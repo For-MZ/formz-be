7ce0f7b828d94e0ebe1e1d313dec8b8dde943a6d
lalabulla <dlrbgus722@naver.com>
2024-03-02 00:40:08 +0900

    CHORE : 주석 해제

--- Server/src/main/java/ForMZ/Server/domain/comment/entity/Comment.java
+++ Server/src/main/java/ForMZ/Server/domain/comment/entity/Comment.java
@@ -1,10 +1,15 @@
 package ForMZ.Server.domain.comment.entity;
 
+import ForMZ.Server.domain.commentLike.entity.CommentLike;
+import ForMZ.Server.domain.post.entity.Post;
 import ForMZ.Server.domain.user.entity.User;
 import ForMZ.Server.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,13 +28,13 @@ public class Comment extends BaseEntity {
     @JoinColumn(name = "USER_ID", nullable = false)
     private User user;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "POST_ID")
-//    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
+    private Post post;
 
-//    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
-//    private List<Comment> commentChild = new ArrayList<>();
+    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    private List<Comment> commentChilds = new ArrayList<>();
 
-//    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
-//    private List<CommentLike> commentLikes = new ArrayList<>();
+    @OneToMany(mappedBy = "comment", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    private List<CommentLike> commentLikes = new ArrayList<>();
 }
