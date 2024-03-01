e9322b3e066e9be91137549172db2134704e2e59
lalabulla <dlrbgus722@naver.com>
2024-03-02 00:34:27 +0900

    Fix : Integer -> Long 변경

--- Server/src/main/java/ForMZ/Server/domain/user/repository/UserRepository.java
+++ Server/src/main/java/ForMZ/Server/domain/user/repository/UserRepository.java
@@ -3,5 +3,5 @@ package ForMZ.Server.domain.user.repository;
 import ForMZ.Server.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
