8368f9ae2b2f74228366098525f633c93238753d
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-02 14:39:01 +0900

    Fix : Repository 타입 Integer -> Long 으로 변경

--- Server/src/main/java/ForMZ/Server/domain/post/repository/PostRepository.java
+++ Server/src/main/java/ForMZ/Server/domain/post/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package ForMZ.Server.domain.post.repository;
 
-public class PostRepository {
+import ForMZ.Server.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
