9ffaae48ba1fb0655ea2111abff470170bd0a203
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-02 14:58:50 +0900

    Feat : 카테고리 코드 수정 및 유효성 검증 추가

--- Server/src/main/java/ForMZ/Server/domain/category/entity/Category.java
+++ Server/src/main/java/ForMZ/Server/domain/category/entity/Category.java
@@ -1,8 +1,13 @@
 package ForMZ.Server.domain.category.entity;
 
+import ForMZ.Server.domain.category.exception.CategoryExceptionCode;
+import ForMZ.Server.domain.category.exception.CategoryLogicException;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.stream.Stream;
 
 @Entity
 @Getter
@@ -17,11 +22,15 @@ public class Category {
     @Column(nullable = false)
     private CategoryCode categoryCode;
 
-    public enum CategoryCode{
-        YOUTH_POLICY,
-        YOUTH_SPACE,
-        HAPPY_HOUSE,
-        EMPLOYMENT,
-        FOUNDED
+    public enum CategoryCode {
+        POLICY, HOUSE, EMPLOYMENT, FOUNDED, FREEDOM, HONEYTIP;
+
+        @JsonCreator
+        public static CategoryCode parsing(String inputValue) {
+            return Stream.of(CategoryCode.values())
+                    .filter(categoryCode -> categoryCode.toString().equals(inputValue.toUpperCase()))
+                    .findFirst()
+                    .orElseThrow(() -> new CategoryLogicException(CategoryExceptionCode.INVALID_CATEGORY_CODE));
+        }
     }
 }
