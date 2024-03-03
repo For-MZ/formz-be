69e5f6755208b599a49c9ff71776ea817e997ebf
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-03 12:59:01 +0900

    Feat : update 위한 Setter 추가

--- Server/src/main/java/ForMZ/Server/domain/post/entity/Post.java
+++ Server/src/main/java/ForMZ/Server/domain/post/entity/Post.java
@@ -8,12 +8,14 @@ import ForMZ.Server.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Post extends BaseEntity {
 
