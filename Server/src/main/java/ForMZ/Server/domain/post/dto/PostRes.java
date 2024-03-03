b8b2cbc55c7ddf7603c65c45e5cd5e16a9904b2f
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-03 12:59:54 +0900

    Feat : PostReq, PostRes Dto 작성

---
+++ Server/src/main/java/ForMZ/Server/domain/post/dto/PostRes.java
@@ -0,0 +1,13 @@
+package ForMZ.Server.domain.post.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRes {
+    private int postId;
+}
