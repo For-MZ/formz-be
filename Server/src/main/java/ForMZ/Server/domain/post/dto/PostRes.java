75e01753f8465d72b229f86017bdd69670476e74
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-07 18:18:48 +0900

    Feat : PostRes Dto 작성

--- Server/src/main/java/ForMZ/Server/domain/post/dto/PostRes.java
+++ Server/src/main/java/ForMZ/Server/domain/post/dto/PostRes.java
@@ -1,10 +1,13 @@
 package ForMZ.Server.domain.post.dto;
 
-import jakarta.validation.constraints.NotNull;
+import ForMZ.Server.domain.category.entity.Category;
+import ForMZ.Server.domain.comment.dto.AllCommentRes;
+import ForMZ.Server.domain.post.entity.Content;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -12,4 +15,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PostRes {
     private int postId;
+    private String title;
+    private Content content;
+    private String writer;
+    private Category category;
+    private boolean bookmarked;
+    private boolean liked;
+    private int likeCnt;
+    private int viewCnt;
+    private int commentCnt;
+    private AllCommentRes comments;
 }
