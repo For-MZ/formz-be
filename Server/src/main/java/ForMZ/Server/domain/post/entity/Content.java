e61326ebf6cbb5979b54418cedda7e417645a911
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-07 18:17:37 +0900

    Chore : 임베디드 타입 Content 내부 애너테이션 추가

--- Server/src/main/java/ForMZ/Server/domain/post/entity/Content.java
+++ Server/src/main/java/ForMZ/Server/domain/post/entity/Content.java
@@ -2,8 +2,16 @@ package ForMZ.Server.domain.post.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Content {
     @Column
     private String text;
