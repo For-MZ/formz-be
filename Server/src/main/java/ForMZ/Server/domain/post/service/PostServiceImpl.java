56c51d3d5a3a29e296b73f514bd350176e3e9265
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-07 18:21:05 +0900

    Feat : 1차 기능 구현 및 내부 메서드 리팩토링 추가

--- Server/src/main/java/ForMZ/Server/domain/post/service/PostServiceImpl.java
+++ Server/src/main/java/ForMZ/Server/domain/post/service/PostServiceImpl.java
@@ -1,20 +1,151 @@
 package ForMZ.Server.domain.post.service;
 
+import ForMZ.Server.domain.bookmark.entity.Bookmark;
+import ForMZ.Server.domain.bookmark.repository.BookmarkRepository;
+import ForMZ.Server.domain.post.dto.PostRes;
 import ForMZ.Server.domain.post.entity.Post;
 import ForMZ.Server.domain.post.exception.PostNotFoundException;
+import ForMZ.Server.domain.post.exception.UnauthorizedPostAccessException;
+import ForMZ.Server.domain.post.mapper.PostMapper;
 import ForMZ.Server.domain.post.repository.PostRepository;
+import ForMZ.Server.domain.postLike.entity.PostLike;
+import ForMZ.Server.domain.postLike.repository.PostLikeRepository;
+import ForMZ.Server.domain.user.entity.User;
+import ForMZ.Server.domain.user.service.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static ForMZ.Server.global.entity.BaseEntity.ObjectState.*;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+    private final BookmarkRepository bookmarkRepository;
+    private final PostLikeRepository postLikeRepository;
+    private final UserService userService;
+    private final PostMapper mapper;
+
+    /**
+     *  POST 생성
+     */
+    @Override
+    @Transactional
+    public Post createPost(Post post){
+        return postRepository.save(post);
+    }
+
+    /**
+     *  1개의 POST 조회
+     */
     @Override
     public Post getPost(Long postId) {
         Post post =  postRepository.findById(postId)
                 .orElseThrow(PostNotFoundException::new);
+
+        if(post.getObjectState() == DEL)
+            throw new PostNotFoundException();
         return post;
+    }
+
+    /**
+     *  POST 수정
+     */
+    @Override
+    @Transactional
+    public Post updatePost(Post modPost, Long postId){
+        Post post = getPost(postId);
+        verifyPostAuthority(post);
+
+        editField(modPost::getCategory, post::setCategory);
+        editField(modPost::getTitle, post::setTitle);
+        editField(modPost::getContent, post::setContent);
+
+        return postRepository.save(post);
+    }
+
+    /**
+     *  POST 삭제
+     */
+    @Override
+    @Transactional
+    public void deletePost(Long postId){
+        Post post = getPost(postId);
+        verifyPostAuthority(post);
+        post.changeState(DEL);
+    }
+
+    /**
+     *  모든 POST 조회
+     */
+    @Override
+    public Page<Post> getPosts(String sortParam, int page, int size){   // sortParam : 정렬 기준
+        // TODO: sortParam에 맞는 정렬 작업
+        return postRepository.findAll(PageRequest.of(page, size,
+                Sort.by("createdDate").descending()));
+    }
+
+    /**
+     *  POST -> POST_RES 변경해주는 메서드
+     */
+    public PostRes convertPostRes(Post post){
+        Long userId = 0L;   // TODO: 리프레시 토큰에서 현재 사용자 id 가져오는 메서드
+        User user = userService.getUser(userId);
+        boolean bookmarked = checkUsersBookmarkedPost(post, user);
+        boolean liked = checkUsersPostLike(post, user);
+        /**
+         *  TODO: 북마크와 좋아요 확인 User.postLike, User.bookmark 에서 찾는게 나은지, 각각의 Repository에서 찾는게 나은지
+         */
+        int likeCnt = post.getPostLikes().size();
+        return mapper.postToPostRes(post, bookmarked, liked, likeCnt);
+    }
+
+    /**
+     *  현재 유저가 북마크한 POST인지 확인
+     */
+    private boolean checkUsersBookmarkedPost(Post post, User user) {
+        boolean bookmarked = false;
+        Optional<Bookmark> bookmark = bookmarkRepository.findByUserAndPost(user, post);
+        if(bookmark.isPresent()){
+            bookmarked = true;
+        }
+        return bookmarked;
+    }
+
+    /**
+     *  현재 유저가 좋아요 누른 POST인지 확인
+     */
+    private boolean checkUsersPostLike(Post post, User user) {
+        boolean liked = false;
+        Optional<PostLike> postLike = postLikeRepository.findByUserAndPost(user, post);
+        if(postLike.isPresent()){
+            liked = true;
+        }
+        return liked;
+    }
+
+    /**
+     *  현재 사용자가 해당 POST에 대한 권한을 가지고 있는지 확인
+     */
+    private void verifyPostAuthority(Post post){
+        Long userId = 0L;   // TODO: 리프레시 토큰에서 현재 사용자 id 가져오는 메서드
+        if(!post.getUser().getId().equals(userId)){
+            throw new UnauthorizedPostAccessException();
+        }
+    }
+
+
+
+    private <T> void editField(Supplier<T> supplier, Consumer<T> consumer) {
+        Optional.ofNullable(supplier.get()).ifPresentOrElse(consumer, () -> consumer.accept(null));
     }
 }
