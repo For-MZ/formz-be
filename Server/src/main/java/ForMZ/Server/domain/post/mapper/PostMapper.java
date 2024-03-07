21508c72d139d417d626ac298511961ffeca56c9
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-07 18:24:22 +0900

    Feat : Post Mapper 1차 구현

--- Server/src/main/java/ForMZ/Server/domain/post/mapper/PostMapper.java
+++ Server/src/main/java/ForMZ/Server/domain/post/mapper/PostMapper.java
@@ -1,20 +1,27 @@
 package ForMZ.Server.domain.post.mapper;
 
+import ForMZ.Server.domain.comment.mapper.CommentMapper;
 import ForMZ.Server.domain.post.dto.PostReq;
 import ForMZ.Server.domain.post.dto.PostRes;
 import ForMZ.Server.domain.post.entity.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
 
 import java.util.List;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = "spring", uses = CommentMapper.class)
 public interface PostMapper {
     @Mapping(source = "postReq.category", target = "category.categoryCode")
     Post postReqToPost(PostReq postReq);
 
     @Mapping(source = "post.id", target = "postId")
-    PostRes postToPostRes(Post post);
+    @Mapping(source = "post.content", target = "content")
+    @Mapping(source = "post.user.nickName", target = "writer")
+    @Mapping(source = "post.category.categoryCode", target = "category")
+    @Mapping(source = "bookmarked", target = "bookmarked")
+    @Mapping(source = "liked", target = "liked")
+    @Mapping(source = "likeCnt", target = "likeCnt")
+    PostRes postToPostRes(Post post, boolean bookmarked, boolean liked, int likeCnt);
+
     List<PostRes> postListToPostResList(List<Post> posts);
 }
