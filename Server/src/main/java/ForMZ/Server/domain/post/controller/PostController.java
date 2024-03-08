64d4a0834559bf5bc6a7be9361ee99e7318f7481
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-08 12:41:50 +0900

    Refactor : ResponseEntity return 형식 변경

--- Server/src/main/java/ForMZ/Server/domain/post/controller/PostController.java
+++ Server/src/main/java/ForMZ/Server/domain/post/controller/PostController.java
@@ -8,11 +8,9 @@ import ForMZ.Server.domain.post.mapper.PostMapper;
 import ForMZ.Server.domain.post.service.PostService;
 import ForMZ.Server.global.common.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,13 +18,12 @@ import java.util.List;
 import static ForMZ.Server.domain.post.constant.PostConstant.PostResponseMessage.*;
 
 @RestController
-@Slf4j
-@Validated
 @RequestMapping("/community")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class PostController {
 
+    // Mapper 사용은 Service Layer 에서만으로 통일 고려
     private final PostMapper mapper;
     private final PostService postService;
 
@@ -37,7 +34,7 @@ public class PostController {
     public ResponseEntity createPost(@RequestBody PostReq postReq){
         Post post = postService.createPost(mapper.postReqToPost(postReq));
         PostRes res = postService.convertPostRes(post);
-        return new ResponseEntity(ResponseDto.create(201, CREATE_POST_SUCCESS.getMessage(), res), HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(HttpStatus.CREATED.value(), CREATE_POST_SUCCESS.getMessage(), res));
     }
 
     /**
@@ -48,7 +45,7 @@ public class PostController {
                                      @PathVariable("postId") Long postId){
         Post post = postService.updatePost(mapper.postReqToPost(postReq), postId);
         PostRes res = postService.convertPostRes(post);
-        return new ResponseEntity(ResponseDto.create(200, UPDATE_POST_SUCCESS.getMessage(), res), HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(HttpStatus.OK.value(), UPDATE_POST_SUCCESS.getMessage(), res));
     }
 
     /**
@@ -57,29 +54,29 @@ public class PostController {
     @DeleteMapping("/posts/{postId}")
     public ResponseEntity deletePost(@PathVariable("postId") Long postId){
         postService.deletePost(postId);
-        return new ResponseEntity(ResponseDto.create(204, DELETE_POST_SUCCESS.getMessage()),HttpStatus.NO_CONTENT);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ResponseDto.create(HttpStatus.NO_CONTENT.value(), DELETE_POST_SUCCESS.getMessage()));
     }
 
     /**
      *  게시글 목록 조회
      */
     @GetMapping("/posts")
-    public ResponseEntity checkPostList(@RequestParam(name = "page", defaultValue = "1", required = false) int page,
+    public ResponseEntity getPostList(@RequestParam(name = "page", defaultValue = "1", required = false) int page,
                                       @RequestParam(name = "pageSize", defaultValue = "10", required = false) int size,
                                       @RequestParam(name = "category", required = false) String category,
                                         @RequestParam(name = "sortParam", defaultValue = "createdDate", required = false) String sortParam){
         Page<Post> postList = postService.getPosts(sortParam, category, page-1, size);
         List<PostRes> postResList = mapper.postListToPostResList(postList.getContent());
-        return new ResponseEntity(ResponseDto.create(200, GET_POSTS_SUCCESS.getMessage(), postResList), HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(HttpStatus.OK.value(), GET_POSTS_SUCCESS.getMessage(), postResList));
     }
 
     /**
      *  특정 게시글 개별 조회
      */
     @GetMapping("/posts/{postId}")
-    public ResponseEntity checkPost(@PathVariable("postId") Long postId){
+    public ResponseEntity getPost(@PathVariable("postId") Long postId){
         Post post = postService.getPost(postId);
         PostRes res = postService.convertPostRes(post);
-        return new ResponseEntity(ResponseDto.create(200, GET_POST_SUCCESS.getMessage(), res),HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(HttpStatus.OK.value(), GET_POST_SUCCESS.getMessage(), res));
     }
 }
