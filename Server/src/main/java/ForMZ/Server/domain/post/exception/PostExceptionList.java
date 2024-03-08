3a6d68b74b3cf97c3f4d2ed9c81b941de31a9301
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-08 12:36:58 +0900

    Fix : UNAUTHORIZED_POST_ACCESS Exception 메시지 변경

--- Server/src/main/java/ForMZ/Server/domain/post/exception/PostExceptionList.java
+++ Server/src/main/java/ForMZ/Server/domain/post/exception/PostExceptionList.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PostExceptionList {
     POST_NOT_FOUND("PO01", HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
-    UNAUTHORIZED_POST_ACCESS("PO02", HttpStatus.UNAUTHORIZED, "허용되지 않은 접근입니다."),
+    UNAUTHORIZED_POST_ACCESS("PO02", HttpStatus.UNAUTHORIZED, "게시물 접근 권한이 없습니다."),
     INVALID_SORT_PARAM("PO03", HttpStatus.BAD_REQUEST, "올바르지 않은 정렬 기준입니다.");
 
     private final String statusCode;
