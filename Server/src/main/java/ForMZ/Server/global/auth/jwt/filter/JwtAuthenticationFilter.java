61e547a4d451c693f5a36bb54173e0630405d0ee
pis03133 <pis03133@naver.com>
2024-03-10 14:37:29 +0900

    Feat: jwt provider 생성

--- Server/src/main/java/ForMZ/Server/global/auth/jwt/filter/JwtAuthenticationFilter.java
+++ Server/src/main/java/ForMZ/Server/global/auth/jwt/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,52 @@
 package ForMZ.Server.global.auth.jwt.filter;
 
-public class JwtAuthenticationFilter {
+import ForMZ.Server.domain.user.dto.request.LoginReq;
+import ForMZ.Server.global.auth.jwt.tokenizer.JwtTokenProvider;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private static final String SECRET_KEY = "your-secret-key";
+    private static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        System.out.println("로그인 시도 : jwt autb filter");
+        ObjectMapper om = new ObjectMapper();
+        try{
+            LoginReq login = om.readValue(request.getInputStream(), LoginReq.class);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(login.getEmail(), login.getPassword());
+            System.out.println("2 : jwt autb filter");
+            System.out.println("3 : jwt autb filter");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
+
