a9d70eb20a98a057d81e873f847a056cfc9f9577
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-11 16:54:15 +0900

    Feat : 조회 수 명칭 변경, 게시글 수정 부분 변경, 게시글 리스트 조회 시 카테고리 필터링 추가

--- Server/src/main/java/ForMZ/Server/domain/post/service/PostService.java
+++ Server/src/main/java/ForMZ/Server/domain/post/service/PostService.java
@@ -1,17 +1,17 @@
 package ForMZ.Server.domain.post.service;
 
+import ForMZ.Server.domain.post.dto.AllPostRes;
 import ForMZ.Server.domain.post.dto.PostReq;
 import ForMZ.Server.domain.post.dto.PostRes;
+import ForMZ.Server.domain.post.dto.PostUpdateReq;
 import ForMZ.Server.domain.post.entity.Post;
-import org.springframework.data.domain.Page;
-
-import java.util.List;
 
 public interface PostService {
-    void createPost(Post post);
+    void createPost(PostReq postReq);
     Post getPost(Long postId);
-    Post updatePost(Post post, Long postId);
+    Post updatePost(PostUpdateReq postUpdateReq, Long postId);
     void deletePost(Long postId);
-    Page<Post> getPosts(String sortParam, String category, int page, int size);
+    AllPostRes getPosts(String sort, String categoryCode, int page, int size);
     PostRes convertPostRes(Post post);
+    void viewPlus(Post post);
 }
