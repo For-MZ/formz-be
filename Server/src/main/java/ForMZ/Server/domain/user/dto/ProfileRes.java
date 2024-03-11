f6375cdd7fbf032920de71673c5b358ae823f02b
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-11 17:51:08 +0900

    Fix : 유저 프로필 이미지 매핑 추가

--- Server/src/main/java/ForMZ/Server/domain/user/dto/ProfileRes.java
+++ Server/src/main/java/ForMZ/Server/domain/user/dto/ProfileRes.java
@@ -1,10 +1,12 @@
 package ForMZ.Server.domain.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProfileRes {
--- Server/src/main/java/ForMZ/Server/domain/user/mapper/UserMapper.java
+++ Server/src/main/java/ForMZ/Server/domain/user/mapper/UserMapper.java
@@ -3,8 +3,10 @@ package ForMZ.Server.domain.user.mapper;
 import ForMZ.Server.domain.user.dto.ProfileRes;
 import ForMZ.Server.domain.user.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(source = "user.profileImage.fileUrl", target = "profileImage")
     ProfileRes userToProfileRes(User user);
 }
