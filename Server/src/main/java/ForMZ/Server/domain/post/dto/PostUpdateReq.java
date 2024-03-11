c304feb1fc814ebf59d369de0c7f3622948dd116
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-11 16:56:42 +0900

    Feat : 게시글 수정 DTO 따로 추가 생성

---
+++ Server/src/main/java/ForMZ/Server/domain/post/dto/PostUpdateReq.java
@@ -0,0 +1,20 @@
+package ForMZ.Server.domain.post.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostUpdateReq {
+
+    private String category;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String text;
+
+    private String imageUrl;
+}
