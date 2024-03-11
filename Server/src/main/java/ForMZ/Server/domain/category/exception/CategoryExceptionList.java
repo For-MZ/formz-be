58756f8e2dbbd76b2f347adf4f8fcb2abec29171
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-11 17:10:13 +0900

    Chore : Category Exception Code CA -> CT로 변경

--- Server/src/main/java/ForMZ/Server/domain/category/exception/CategoryExceptionList.java
+++ Server/src/main/java/ForMZ/Server/domain/category/exception/CategoryExceptionList.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CategoryExceptionList {
-    CATEGORY_NOT_FOUND("CA01", HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다.");
+    CATEGORY_NOT_FOUND("CT01", HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다.");
 
     private final String statusCode;
     private final HttpStatus httpStatus;
