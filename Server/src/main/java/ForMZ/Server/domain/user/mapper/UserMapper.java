e2721485e6fb720f44ac7b2cbc0aa60c73348d2f
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-11 16:36:02 +0900

    Feat : User To ProfileRes 매핑 추가

--- Server/src/main/java/ForMZ/Server/domain/user/mapper/UserMapper.java
+++ Server/src/main/java/ForMZ/Server/domain/user/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package ForMZ.Server.domain.user.mapper;
 
+import ForMZ.Server.domain.user.dto.ProfileRes;
+import ForMZ.Server.domain.user.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    ProfileRes userToProfileRes(User user);
 }
