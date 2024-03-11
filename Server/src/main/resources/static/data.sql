99c511c78bbe3fbf48a189e24dfbed5ab98a0265
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-11 17:13:45 +0900

    Feat : 카테고리 초기 데이터 값 설정

---
+++ Server/src/main/resources/static/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO CATEGORY(category_code, created_date, last_modified_date, object_state)
+    VALUES('POLICY', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACT');
+INSERT INTO CATEGORY(category_code, created_date, last_modified_date, object_state)
+    VALUES('HOUSE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACT');
+INSERT INTO CATEGORY(category_code, created_date, last_modified_date, object_state)
+    VALUES('EMPLOYMENT', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACT');
+INSERT INTO CATEGORY(category_code, created_date, last_modified_date, object_state)
+    VALUES('FOUNDED', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACT');
+INSERT INTO CATEGORY(category_code, created_date, last_modified_date, object_state)
+    VALUES('FREEDOM', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACT');
+INSERT INTO CATEGORY(category_code, created_date, last_modified_date, object_state)
+    VALUES('HONEY_TIP', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACT');
