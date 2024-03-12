12389055a6489a0cb700e60db2a09b3fcc552418
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-12 22:46:34 +0900

    Feat : UserDetails 구현, refreshToken 컬럼 추가, roles 컬럼 수정

--- Server/src/main/java/ForMZ/Server/domain/user/entity/User.java
+++ Server/src/main/java/ForMZ/Server/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package ForMZ.Server.domain.user.entity;
 import ForMZ.Server.domain.bookmark.entity.Bookmark;
 import ForMZ.Server.domain.comment.entity.Comment;
 import ForMZ.Server.domain.commentLike.entity.CommentLike;
+import ForMZ.Server.domain.file.entity.File;
 import ForMZ.Server.domain.post.entity.Post;
 import ForMZ.Server.domain.postLike.entity.PostLike;
 import ForMZ.Server.global.entity.BaseEntity;
@@ -11,15 +12,20 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
-public class User extends BaseEntity {
+@AllArgsConstructor
+public class User extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -33,18 +39,19 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
-    @Column
+    @Column(nullable = false)
     private String nickName;
 
-    @Enumerated(value = EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 
     @Enumerated(value = EnumType.STRING)
     @Column(nullable = false)
     private SignType signType;
 
-
+    @Column
+    private String refreshToken;
 
     @OneToMany(mappedBy = "user", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
     private List<Comment> comments = new ArrayList<>();
@@ -61,30 +68,49 @@ public class User extends BaseEntity {
     @OneToMany(mappedBy = "user", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
     private List<PostLike> postLikes = new ArrayList<>();
 
-    public User(String subject, String s, Collection<? extends GrantedAuthority> authorities) {
-        this.email = subject;
-        this.password = s;
-        //this.role = authorities;
-    }
-
-    public enum Role implements GrantedAuthority {
-        USER,
-        ADMIN;
-
-        @Override
-        public String getAuthority() {
-            return null;
-        }
-    }
-
     public enum SignType{
         NORMAL,
         GOOGLE,
         KAKAO
     }
 
-    public Role getRole(){
-        return this.role;
+    public void updateRefreshToken(String refreshToken){
+        this.refreshToken = refreshToken;
     }
 
+    /**
+     * UserDetails Implements
+     */
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
