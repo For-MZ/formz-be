0a73f07bfba817947eba01f8dadcd38c5308679e
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-12 23:07:32 +0900

    Feat : login POST 요청 메서드 추가

--- Server/src/main/java/ForMZ/Server/domain/user/controller/UserController.java
+++ Server/src/main/java/ForMZ/Server/domain/user/controller/UserController.java
@@ -1,27 +1,27 @@
 package ForMZ.Server.domain.user.controller;
 
-import ForMZ.Server.domain.post.dto.PostReq;
+import ForMZ.Server.domain.user.service.UserService;
+import ForMZ.Server.global.auth.jwt.dto.JwtToken;
+import ForMZ.Server.global.auth.jwt.dto.LoginReq;
 import ForMZ.Server.global.common.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static ForMZ.Server.domain.post.constant.PostConstant.PostResponseMessage.CREATE_POST_SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-    // Spring security 무한 redirection 방지용 임시 login controller
-    @RequestMapping("/login")
-    public ResponseEntity login(){
+    private final UserService userService;
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginReq loginReq){
+        JwtToken jwtToken = userService.Login(loginReq);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(HttpStatus.CREATED.value(), "로그인에 성공하였습니다", jwtToken));
     }
 
     @RequestMapping("/login/oauth")
