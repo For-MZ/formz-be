7a7cbfc750259b48f9017780f15d9a58bb9a8824
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-12 22:52:36 +0900

    Feat : email로 User 식별하여 UserDetails 객체 생성 및 반환 메서드

--- Server/src/main/java/ForMZ/Server/global/auth/jwt/service/PrincipalDetailsService.java
+++ Server/src/main/java/ForMZ/Server/global/auth/jwt/service/PrincipalDetailsService.java
@@ -3,25 +3,36 @@ package ForMZ.Server.global.auth.jwt.service;
 import ForMZ.Server.domain.user.entity.User;
 import ForMZ.Server.domain.user.exception.UserNotFoundException;
 import ForMZ.Server.domain.user.repository.UserRepository;
-import ForMZ.Server.global.auth.jwt.dto.PrincipalDetails;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         System.out.println("PrincipalDetailsService");
         System.out.println("Login");
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException());;
-        return null;
-                //new PrincipalDetails(user);
+
+        return userRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UserNotFoundException());
+    }
+
+    // 해당하는 User가 존재한다면 UserDetails 객체로 만들어서 return
+    private UserDetails createUserDetails(User user){
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .roles(user.getRoles().toArray(new String[0]))
+                .build();
     }
 }
