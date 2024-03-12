b65ee173acf83184f642d96c97c8076c29e62c33
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-12 22:43:36 +0900

    Chore : import문 정리

--- Server/src/main/java/ForMZ/Server/domain/user/repository/UserRepository.java
+++ Server/src/main/java/ForMZ/Server/domain/user/repository/UserRepository.java
@@ -1,15 +1,12 @@
 package ForMZ.Server.domain.user.repository;
 
 import ForMZ.Server.domain.user.entity.User;
-import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByEmail(String email);
 }
