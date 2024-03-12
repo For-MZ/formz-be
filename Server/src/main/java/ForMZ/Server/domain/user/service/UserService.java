054d8a97ecd5c1ba3dca6395577c4240957cf56f
tkfkdgowksel <tkfkdgowksel@gmail.com>
2024-03-12 22:41:42 +0900

    Feat: UserService Login 메서드 추가

--- Server/src/main/java/ForMZ/Server/domain/user/service/UserService.java
+++ Server/src/main/java/ForMZ/Server/domain/user/service/UserService.java
@@ -1,7 +1,10 @@
 package ForMZ.Server.domain.user.service;
 
 import ForMZ.Server.domain.user.entity.User;
+import ForMZ.Server.global.auth.jwt.dto.JwtToken;
+import ForMZ.Server.global.auth.jwt.dto.LoginReq;
 
 public interface UserService {
     User getUser(Long userId);
+    JwtToken Login(LoginReq loginReq);
 }
--- Server/src/main/java/ForMZ/Server/domain/user/service/UserServiceImpl.java
+++ Server/src/main/java/ForMZ/Server/domain/user/service/UserServiceImpl.java
@@ -3,13 +3,22 @@ package ForMZ.Server.domain.user.service;
 import ForMZ.Server.domain.user.entity.User;
 import ForMZ.Server.domain.user.exception.UserNotFoundException;
 import ForMZ.Server.domain.user.repository.UserRepository;
+import ForMZ.Server.global.auth.jwt.dto.JwtToken;
+import ForMZ.Server.global.auth.jwt.dto.LoginReq;
+import ForMZ.Server.global.auth.jwt.tokenizer.JwtTokenProvider;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public User getUser(Long userId) {
@@ -17,5 +26,24 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(UserNotFoundException::new);
         return user;
     }
-  
+
+    @Override
+    @Transactional
+    public JwtToken Login(LoginReq loginReq) {
+
+        final String email = loginReq.getEmail();
+        final String password = loginReq.getPassword();
+
+        // email + password 기반으로 Authentication 객체 생성
+        // 이 때, authentication 은 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+
+        // 실제 검증. authenticate() 메서드를 통해 요청된 User에 대한 검증 진행
+        // authenticate 메서드가 실행될 때 CustomUserDetailsService에서 만든 loadUserByUsername 메서드 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 인증 정보를 기반으로 JWT 토큰 생성
+        return jwtTokenProvider.generateToken(authentication);
+    }
+
 }
